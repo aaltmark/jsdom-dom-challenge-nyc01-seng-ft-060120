19cb39d444aebbf7c2be4ac302d39f499d5f61e6
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-07-15 15:11:40 -0400

    maleeha's part

--- js/challenge.js
+++ js/challenge.js
@@ -11,10 +11,10 @@
 document.addEventListener("DOMContentLoaded", function(e){
     const minusButton = document.getElementById("minus");
     const plusButton = document.getElementById("plus");
-    const heartButton = document.getElementById("heart");
+    //const heartButton = document.getElementById("heart");
     const pauseButton = document.getElementById("pause");
     const counter = document.getElementById("counter");
-    const form = document.getElementById("comment-form")
+    //const form = document.getElementById("comment-form")
     const comment = document.getElementById("comment-input")
     const commentList = document.getElementById("list")
 
@@ -26,18 +26,51 @@ document.addEventListener("DOMContentLoaded", function(e){
         counter.textContent = parseInt(counter.textContent, 10) - 1 
     })
 
-    form.addEventListener("submit", function(e){
-        e.preventDefault
-        const addCommentTag = document.createElement("p"); 
-        addCommentTag.textContent = comment 
-        addCommentTag.append(commentList);
-    })
 
-    //function addComment(newComment){
-        //newComment.preventDefault(); 
-        //const addCommentTag = document.createElement("p"); 
-        //addCommentTag.textContent = comment 
-        //addCommentTag.append(commentList);
-        //commentList.target.reset 
-    //}
+    const form = document.getElementById("comment-form")
+    form.addEventListener("submit", addComment)
+
+    function addComment(newComment){
+        newComment.preventDefault(); 
+        const addCommentTag = document.createElement("p"); 
+        addCommentTag.textContent = comment.value
+        commentList.appendChild(addCommentTag);
+        form.reset();
+    }
+
+
+//create an event listener 
+// an li under the ul for likes 
+// a span for each li 
+//
+
+const heartButton = document.getElementById("heart");
+
+heartButton.addEventListener("click", function(e){
+    const heartLi = document.createElement("li");
+    const heartSpan = document.createElement("span"); 
+    
+    heartSpan.innerHTML = {
+        counter = 0
+        heartButton.onclick = function() {
+        counter += 1
+    }}
+    
+    heartLi.innerHTML = `${counter.innerHTML} has been liked ${heartSpan.innerHTML}`
+    console.dir(heartLi.innerHTML)
 })
+
+
+})
+
+
+
+
+//   count = 0;
+//   heartButton.onclick = function() {
+//   count += 1;
+//   heartSpan.innerHTML = count;
+// };
+
+
+
